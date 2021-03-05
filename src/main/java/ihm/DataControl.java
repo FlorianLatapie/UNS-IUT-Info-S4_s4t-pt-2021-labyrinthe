1d5de6b29d7cd1d8b9533f0743677b7d87b37e89
Florian LATAPIE <70631622+FlorianLatapie@users.noreply.github.com>
2021-03-05 16:26:04 +0100

    ajout d'images supplémentaires

--- src/main/java/ihm/DataControl.java
+++ src/main/java/ihm/DataControl.java
@@ -13,10 +13,19 @@ public class DataControl {
 	public static final String ROBOT = "Images/icon_robot.png";
 	// Images Labyrinthe 
 	
-	public static final String LIGNE0 = "Images/Labyrinthe/ligne0.png";
-	public static final String LIGNE1 = "Images/Labyrinthe/ligne1.png";
-	public static final String LIGNE2 = "Images/Labyrinthe/ligne2.png";
-	public static final String LIGNE3 = "Images/Labyrinthe/ligne3.png";
+	public static final String LIGNE_HAUT = "Images/Labyrinthe/ligne0.png";
+	public static final String LIGNE_DROITE = "Images/Labyrinthe/ligne1.png";
+	public static final String LIGNE_BAS = "Images/Labyrinthe/ligne2.png";
+	public static final String LIGNE_GAUCHE = "Images/Labyrinthe/ligne3.png";
+	
+	public static final String DLIGNE_VERT = "Images/Labyrinthe/2ligne0.png";
+	public static final String DLIGNE_HOR = "Images/Labyrinthe/2ligne1.png";
+	
+	public static final String ANGLE_BAS_GAUCHE = "Images/Labyrinthe/Angle0.png";
+	public static final String ANGLE_HAUT_GAUCHE = "Images/Labyrinthe/Angle1.png";
+	public static final String ANGLE_HAUT_DROITE = "Images/Labyrinthe/Angle2.png";
+	public static final String ANGLE_BAS_DROITE = "Images/Labyrinthe/Angle3.png";
+	
 	public static final String CARRE = "Images/Labyrinthe/Carre.png";
 	
 }
