764d29e2123d9e48b81a9bc0cab6c40bbde5039d
Florian LATAPIE <70631622+FlorianLatapie@users.noreply.github.com>
2021-03-05 16:13:10 +0100

    mise à jour du data controll pour intégrer les images

--- src/main/java/ihm/DataControl.java
+++ src/main/java/ihm/DataControl.java
@@ -10,4 +10,13 @@ public class DataControl {
 	public static final String CSS = "style.css";
 
 	public static final String FOND = "Images/fond.jpg";
+	
+	// Images Labyrinthe 
+	
+	public static final String LIGNE0 = "Images/Labyrinthe/ligne0.png";
+	public static final String LIGNE1 = "Images/Labyrinthe/ligne1.png";
+	public static final String LIGNE2 = "Images/Labyrinthe/ligne2.png";
+	public static final String LIGNE3 = "Images/Labyrinthe/ligne3.png";
+	public static final String CARRE = "Images/Labyrinthe/Carre.png";
+	
 }
