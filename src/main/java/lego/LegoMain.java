150212157a923078814dc9d18a3058c1d31f3219
Florian LATAPIE <70631622+FlorianLatapie@users.noreply.github.com>
2021-03-06 15:14:24 +0100

    création du controleur de mouvement du lego

--- src/main/java/lego/LegoMain.java
+++ src/main/java/lego/LegoMain.java
@@ -1,21 +1,14 @@
 package lego;
 
-import lejos.hardware.motor.Motor;
-import lejos.utility.Delay;
 import lejos.hardware.Button;
 
 public class LegoMain {
 
 	public static void main(String[] args) {
 		System.out.println("Appuyez sur retour pour quitter");
-		
-		while(Button.waitForAnyPress()!=Button.ID_ESCAPE) {
-			if (Button.waitForAnyPress()==Button.ID_UP) {
-				Motor.A.forward();
-				Delay.msDelay(1000);
-				Motor.A.stop();
-			}
-		}
+		MouvementController mc = new MouvementController();
+		mc.methodeExemple();
+		while(Button.waitForAnyPress()!=Button.ID_ESCAPE) {}
 	}
 
 }
