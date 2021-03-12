ba35ddd3b473f70e8d7c6fb365f460b15c6bde71
RemyBldn <70631691+RemyBldn@users.noreply.github.com>
2021-03-12 17:00:00 +0100

    Clean du code

--- src/main/java/raspberry/reseaulego/MultiServerThread.java
+++ src/main/java/raspberry/reseaulego/MultiServerThread.java
@@ -14,31 +14,25 @@ import java.net.Socket;
  *
  */
 public class MultiServerThread implements Runnable {
-	private Thread t; // contiendra le thread du client
-	private Socket s; // recevra le socket liant au client
-	private PrintWriter out; // pour gestion du flux de sortie
-	private BufferedReader in; // pour gestion du flux d'entrée
-	private MultiServer multiServer; // pour utilisation des méthodes de la classe principale
-	private int numClient = 0; // contiendra le numéro de client géré par ce thread
+	private Thread t; 
+	private Socket s; 
+	private PrintWriter out; 
+	private BufferedReader in; 
+	private MultiServer multiServer;
+	private int numClient = 0; 
 
 	public MultiServerThread(Socket socket, MultiServer multiServ) {
 		this.multiServer = multiServ;
 		this.s = socket;
 		try {
-			// fabrication d'une variable permettant l'utilisation du flux de sortie avec
-			// des string
 			out = new PrintWriter(s.getOutputStream());
-			// fabrication d'une variable permettant l'utilisation du flux d'entrée avec des
-			// string
 			in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-			// ajoute le flux de sortie dans la liste et récupération de son numéro
 			numClient = multiServer.addClient(out);
 		} catch (IOException e) {
 			e.getMessage();
 		}
 		t = new Thread(this);
 		t.start();
-		System.out.println("Serveur créé");
 	}
 
 	public void run() {
