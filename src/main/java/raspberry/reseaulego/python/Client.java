3e61256bc88c0d0d17a5f9e9fa12c37337ce8a4d
pierrebihannic <pierre.bihannic@etu.unice.fr>
2021-03-22 17:37:53 +0100

    Modification du serveur python
    traitement et envoie des informations des capteurs line_finder.

--- src/main/java/raspberry/reseaulego/python/Client.java
+++ src/main/java/raspberry/reseaulego/python/Client.java
@@ -1,22 +1,21 @@
+package raspberry.reseaulego.python;
+
 import java.io.*;
 import java.net.*;
 public class Client {
     public static void main(String[] args) throws IOException {
-
-            Socket soc=new Socket("localhost",9000);
-            DataOutputStream dout=new DataOutputStream(soc.getOutputStream());
-            DataInputStream in = new DataInputStream(soc.getInputStream());
-            while(true){
-                String msg=(String)in.readUTF();
-                System.out.println("python: "+msg);
-                dout.writeUTF("Ok");
-                dout.flush();
-                if(msg == "")
-                    break;
-            }
-            dout.close();
-            soc.close();
-
-
+        Socket soc = new Socket("localhost", 9000);
+        DataOutputStream dout = new DataOutputStream(soc.getOutputStream());
+        DataInputStream in = new DataInputStream(soc.getInputStream());
+        while (true) {
+            String msg = (String) in.readUTF();
+            System.out.println("python: " + msg);
+            dout.writeUTF("Ok");
+            dout.flush();
+            if (msg == "")
+                break;
         }
+        dout.close();
+        soc.close();
+    }
 }
