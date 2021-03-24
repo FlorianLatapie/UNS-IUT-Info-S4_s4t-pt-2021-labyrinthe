157b537518391aae3ae8e8112f9e96db251ecfcd
RemyBldn <70631691+RemyBldn@users.noreply.github.com>
2021-03-24 12:20:23 +0100

    Deplacement robot matrice gauche ENFINNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN

--- src/main/java/ihm/TraitementDirection.java
+++ src/main/java/ihm/TraitementDirection.java
@@ -3,6 +3,8 @@
  */
 package ihm;
 
+import java.util.Arrays;
+
 /**
  * @author Remy
  *
@@ -10,9 +12,10 @@ package ihm;
 public class TraitementDirection {
 
 	public static int[] getdCoordByDeplacement(int[] coordRobot, String rotationRobot, String[] deplacement) {
-		System.out.println("yo");
+		System.out.println("coord "+Arrays.toString(coordRobot)+" rota "+ rotationRobot+" deplaceemnt "+ Arrays.toString(deplacement));
 		String rotation = "";
 		String direction = "";
+		int[] res = new int[2];
 		if (deplacement[1] != null) {
 			rotation = deplacement[0];
 			direction = deplacement[1];
@@ -22,25 +25,28 @@ public class TraitementDirection {
 		if (rotationRobot.equals("H") && rotation.equals("G") && direction.equals("A")
 				|| rotationRobot.equals("G") && rotation.equals("A") && direction.equals("")
 				|| rotationRobot.equals("B") && rotation.equals("D") && direction.equals("A")) {
-			coordRobot[1] = (coordRobot[1]) - 1;
-			return coordRobot;
+			res[0] = coordRobot[0];
+			res[1] = (coordRobot[1]) - 1;
 		} else if (rotationRobot.equals("H") && rotation.equals("A") && direction.equals("")
 				|| rotationRobot.equals("G") && rotation.equals("D") && direction.equals("A")
 				|| rotationRobot.equals("D") && rotation.equals("G") && direction.equals("A")) {
-			coordRobot[0] = (coordRobot[0]) - 1;
-			return coordRobot;
+			res[0] = (coordRobot[0]) - 1;
+			res[1] = coordRobot[1];
+			
 		} else if (rotationRobot.equals("H") && rotation.equals("D") && direction.equals("A")
 				|| rotationRobot.equals("D") && rotation.equals("A") && direction.equals("")
 				|| rotationRobot.equals("B") && rotation.equals("G") && direction.equals("A")) {
-			coordRobot[1] = (coordRobot[1]) + 1;
-			return coordRobot;
+			res[0] = coordRobot[0];
+			res[1] = (coordRobot[1]) + 1;
+			
 		} else if (rotationRobot.equals("G") && rotation.equals("G") && direction.equals("A")
 				|| rotationRobot.equals("D") && rotation.equals("D") && direction.equals("A")
 				|| rotationRobot.equals("B") && rotation.equals("A") && direction.equals("")) {
-			coordRobot[0] = (coordRobot[1]) + 1;
-			return coordRobot;
+			res[0] = (coordRobot[0]) + 1;
+			res[1] = coordRobot[1];
+			
 		}
-		return coordRobot;
+		return res;
 	}
 
 }
--- src/main/java/raspberry/reseaupc/ProtocolPC.java
+++ src/main/java/raspberry/reseaupc/ProtocolPC.java
@@ -19,20 +19,16 @@ public class ProtocolPC {
 	}
 
 	public void traitement(String fromServer) {
-		System.out.println("je suis passé "+fromServer);
 		currentCoord = RobotPane.getCurrentCoordRobot();
+		int[] currentTemp = currentCoord;
 		String rotation = RobotPane.getOrientationRobot();
-		System.out.println("Rotation : "+rotation);
 		String[] deplacement = new String[2];
 		if (fromServer.length()<=1)
 			deplacement[0] = fromServer;
 		else
 			deplacement = fromServer.split("");
-		System.out.println("Déplacement : "+Arrays.toString(deplacement));
 		int[] newCoord =  TraitementDirection.getdCoordByDeplacement(currentCoord, rotation, deplacement);
 		if(fromServer != null) {
-			System.out.println("new coord : "+Arrays.toString(newCoord));
-			System.out.println("Current coord : "+Arrays.toString(currentCoord));
 			Evenement.deplacementRobot(currentCoord, newCoord);
 		}
 	}
