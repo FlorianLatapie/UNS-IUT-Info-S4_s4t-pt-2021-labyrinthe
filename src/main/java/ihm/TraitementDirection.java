9451bd37dc0a69a6d789f2c9cf87dd59fe0636f0
RemyBldn <70631691+RemyBldn@users.noreply.github.com>
2021-03-24 10:13:09 +0100

    Correction definitive traitement

--- src/main/java/ihm/TraitementDirection.java
+++ src/main/java/ihm/TraitementDirection.java
@@ -9,33 +9,33 @@ package ihm;
  */
 public class TraitementDirection {
 
-	public int[] getdCoordByDeplacement(int[] coordRobot, String rotationRobot, String[] deplacement) {
-		String rotation = null;
-		String direction = null;
-		if (deplacement.length > 1) {
+	public static int[] getdCoordByDeplacement(int[] coordRobot, String rotationRobot, String[] deplacement) {
+		String rotation = "";
+		String direction = "";
+		if (deplacement[1] != null) {
 			rotation = deplacement[0];
 			direction = deplacement[1];
 		} else {
 			rotation = deplacement[0];
 		}
 		if (rotationRobot.equals("H") && rotation.equals("G") && direction.equals("A")
-				|| rotationRobot.equals("G") && rotation.equals("A") && direction.equals(null)
+				|| rotationRobot.equals("G") && rotation.equals("A") && direction.equals("")
 				|| rotationRobot.equals("B") && rotation.equals("D") && direction.equals("A")) {
 			coordRobot[1] = (coordRobot[1]) - 1;
 			return coordRobot;
-		} else if (rotationRobot.equals("H") && rotation.equals("A") && direction.equals(null)
+		} else if (rotationRobot.equals("H") && rotation.equals("A") && direction.equals("")
 				|| rotationRobot.equals("G") && rotation.equals("D") && direction.equals("A")
 				|| rotationRobot.equals("D") && rotation.equals("G") && direction.equals("A")) {
 			coordRobot[0] = (coordRobot[0]) - 1;
 			return coordRobot;
 		} else if (rotationRobot.equals("H") && rotation.equals("D") && direction.equals("A")
-				|| rotationRobot.equals("D") && rotation.equals("A") && direction.equals(null)
+				|| rotationRobot.equals("D") && rotation.equals("A") && direction.equals("")
 				|| rotationRobot.equals("B") && rotation.equals("G") && direction.equals("A")) {
 			coordRobot[1] = (coordRobot[1]) + 1;
 			return coordRobot;
 		} else if (rotationRobot.equals("G") && rotation.equals("G") && direction.equals("A")
 				|| rotationRobot.equals("D") && rotation.equals("D") && direction.equals("A")
-				|| rotationRobot.equals("B") && rotation.equals("A") && direction.equals(null)) {
+				|| rotationRobot.equals("B") && rotation.equals("A") && direction.equals("")) {
 			coordRobot[0] = (coordRobot[1]) + 1;
 			return coordRobot;
 		}
