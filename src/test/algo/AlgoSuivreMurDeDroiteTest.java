cbf85e5370a7ae2be37c282ebb364c8a6bbd50c7
RemyBldn <70631691+RemyBldn@users.noreply.github.com>
2021-03-25 19:06:45 +0100

    Revert "deplacement les tests vers src/test/java"
    This reverts commit 13811041d11ed86fb609d65d622b1ee93550209c.

---
+++ src/test/algo/AlgoSuivreMurDeDroiteTest.java
@@ -0,0 +1,36 @@
+package algo;
+
+import org.junit.jupiter.api.Test;
+import raspberry.algo.AlgoSuivreMurDeDroite;
+import raspberry.reseau.StaticProtocolMessages;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AlgoSuivreMurDeDroiteTest {
+
+    @Test
+    void executerTest(){
+
+        AlgoSuivreMurDeDroite algo = new AlgoSuivreMurDeDroite();
+
+        //Move forward
+        assertEquals(algo.executer("101"),StaticProtocolMessages.AVANCER);
+        assertEquals(algo.executer("001"),StaticProtocolMessages.AVANCER);
+
+        //Turn left + forward
+        assertEquals(algo.executer("011"),StaticProtocolMessages.GAUCHE+StaticProtocolMessages.AVANCER);
+
+        //Turn around
+        assertEquals(algo.executer("111"),StaticProtocolMessages.GAUCHE+StaticProtocolMessages.GAUCHE);
+
+        //Turn right + forward
+        assertEquals(algo.executer("100"),StaticProtocolMessages.DROITE+StaticProtocolMessages.AVANCER);
+        assertEquals(algo.executer("110"),StaticProtocolMessages.DROITE+StaticProtocolMessages.AVANCER);
+        assertEquals(algo.executer("010"),StaticProtocolMessages.DROITE+StaticProtocolMessages.AVANCER);
+        assertEquals(algo.executer("000"),StaticProtocolMessages.DROITE+StaticProtocolMessages.AVANCER);
+
+        //End of maze
+        assertEquals(algo.executer("000"),StaticProtocolMessages.ARRIVE);
+    }
+
+}
