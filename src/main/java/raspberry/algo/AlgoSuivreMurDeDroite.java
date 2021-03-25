3bedd4977f2ee4191632d2702aa190eae2f80fa1
Jehan-Berthe <70631315+Jehan-Berthe@users.noreply.github.com>
2021-03-25 18:21:03 +0100

    Update AlgoSuivreMurDeDroite.java

--- src/main/java/raspberry/algo/AlgoSuivreMurDeDroite.java
+++ src/main/java/raspberry/algo/AlgoSuivreMurDeDroite.java
@@ -3,31 +3,35 @@ package raspberry.algo;
 import raspberry.reseau.StaticProtocolMessages;
 
 public class AlgoSuivreMurDeDroite implements AlgoStrategy {
-
+	static int arrive = 0;
     @Override
     public String executer(String capteur) {
-        int arrive = 0;
+        
         System.out.println(capteur);
 
         if ((capteur.equals("101")) || (capteur.equals("001"))) {
+        	arrive = 0;
             return StaticProtocolMessages.AVANCER;
         }
 
         else if (capteur.equals("100") || (capteur.equals("110")) || (capteur.equals("010")) || capteur.equals("000")) {
+        	arrive = 0;
             return StaticProtocolMessages.DROITE+StaticProtocolMessages.AVANCER;
         }
 
         else if (capteur.equals("011")) {
+        	arrive = 0;
         	System.out.println("je retourne "+StaticProtocolMessages.GAUCHE+StaticProtocolMessages.AVANCER);
             return StaticProtocolMessages.GAUCHE+StaticProtocolMessages.AVANCER;
 
         } else if (capteur.equals("111")) {
+        	arrive = 0;
             return StaticProtocolMessages.GAUCHE+StaticProtocolMessages.GAUCHE;
         }
 
         else if (capteur.equals("000")) {
             if (arrive == 1) {
-                capteur = null;
+            	return StaticProtocolMessages.DROITE+StaticProtocolMessages.AVANCER;
             } else {
                 arrive = 1;
                 return StaticProtocolMessages.ARRIVE;
